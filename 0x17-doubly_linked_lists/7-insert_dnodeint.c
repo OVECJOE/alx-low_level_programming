62ef796580c68414ecea3439cf44ac980b25f1e1
OVECJOE <ovecjoe123@gmail.com>
2021-09-17 08:07:31 +0100

    Fixed bug in 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,6 +28,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (temp->prev)
 		temp = temp->prev;
 
+	if (!idx)
+	{
+		new_node->next = temp;
+		temp = new_node;
+		return (new_node);
+	}
+
 	while (temp)
 	{
 		if (count == idx - 1)
