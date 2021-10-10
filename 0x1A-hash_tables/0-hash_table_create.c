05412aaca0b117c34049722e810074cac25caf3e
OVECJOE <ovecjoe123@gmail.com>
2021-10-10 07:18:52 +0000

    Fixed betty complains: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,16 +7,15 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *table = NULL;
+	int i;
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-
 	table->size = size;
-	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * table->size);
-	if (table->array == NULL)
+	table->array = (hash_node_t **) malloc(sizeof(hash_node_t *) * table->size);
+	if (!table->array)
 		return (NULL);
 
 	for (i = 0; i < table->size; i++)
