60f1c78336a816ee78d971cd8f9553130a725942
OVECJOE <ovecjoe123@gmail.com>
2021-10-11 06:25:08 +0000

    Added 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: the hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp = NULL;
+	int FLAG = 0;
+
+	if (!ht)
+		return;
+	
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+		if (ht->array[i])
+		{
+			printf("%s", (FLAG) ? ", " : "");
+			tmp = ht->array[i];
+			printf("'%s': ", tmp->key);
+			if (tmp->next)
+			{
+				printf("['%s'", tmp->value);
+				for (tmp = tmp->next; tmp; tmp = tmp->next)
+					printf(", '%s'", tmp->value);
+				printf("]");
+			}
+			else
+				printf("'%s'", tmp->value);
+
+			FLAG = 1;
+		}
+
+	printf("}\n");
+}
+
